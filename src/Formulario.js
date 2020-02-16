66e5edefd2421159f8ddd66fc60c5e3abed80c13
Gabriel <gabriel.developer@outlook.com>
2020-02-16 01:11:09 -0300

    refatorações do codigo realizada

--- src/Formulario.js
+++ src/Formulario.js
@@ -4,14 +4,15 @@ class Formulario extends Component {
 
     constructor(props){
         super(props);
-    
+
         this.stateInicial = {
             nome:'',
             livro:'',
             preco:'',
         }
-    
+
         this.state = this.stateInicial;
+        
     }
 
     escutadorDeInput = event => {
@@ -22,49 +23,46 @@ class Formulario extends Component {
         });
     }
 
+    render(){
 
-    render() {
+        const { nome, livro, preco } = this.state;
 
-        const {nome, livro, preco} = this.state;
-    
-        return (
+        return(
             <form>
-    
-                <label htmlFor="nome">Nome</label>
+
+                <label htmlFor="nome">nome</label>
                 <input
                     id="nome"
                     type="text"
                     name="nome"
                     value={nome}
-                    onChange={this.escutadorDeInput}
+                    onChange = {this.escutadorDeInput}
                 />
-    
+
                 <label htmlFor="livro">Livro</label>
                 <input
                     id="livro"
                     type="text"
                     name="livro"
                     value={livro}
-                    onChange={this.escutadorDeInput}
+                    onChange = {this.escutadorDeInput}
                 />
-    
-    
+
+
                 <label htmlFor="preco">Preço</label>
                 <input
                     id="preco"
                     type="text"
                     name="preco"
                     value={preco}
-                    onChange={this.escutadorDeInput}
+                    onChange = {this.escutadorDeInput}
                 />
-    
-    
+
+
                 <button type="button">Salvar
                 </button>
             </form>
-    
         )
-    
     }
 }
 
