87b256998f9f295c35d32e4db4d2dccf98527570
Gabriel <gabriel.developer@outlook.com>
2020-03-26 02:21:21 -0300

    validação de formluario

--- src/Formulario.js
+++ src/Formulario.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import FormValidator from './FormValidator';
 
 class Formulario extends Component {
 
     constructor(props) {
         super(props);
+
+        this.validador = new FormValidator();
 
         this.stateInicial = {
             nome: '',
@@ -24,8 +27,13 @@ class Formulario extends Component {
     }
 
     submitFormulario = () => {
-        this.props.escutadorDeSubmit(this.state);
-        this.setState(this.stateInicial);
+
+        if(this.validador.valida(this.state)){
+            this.props.escutadorDeSubmit(this.state);
+            this.setState(this.stateInicial);
+        } else {
+            console.log('Submit Bloqueado');
+        }
     }
 
     render() {
